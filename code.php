d34b2bb9adb62fd9354e675ecdf46a858fb47763
Fabrice42 <fabrice.boyer@consultant.com>
2015-09-04 21:05:58 +0400

    Définition des setters de la classe Code

--- code.php
+++ code.php
@@ -4,4 +4,37 @@
 	*
 	* @author Fabrice42 <fabrice42@temp.mail>
 	**/
+namespace \OpenClassrooms;
+
+public class Code
+{
+	protected $version;
+	protected $auteur;
+	protected $exercice;
+	
+	public function __construct($version, $auteur, Exercice $exerice)
+	{
+		if (is_int($version) && ($version > 0)) $this->version = $version;
+		if (is_string($auteur) && strlen($auteur)) $this->auteur = $auteur;
+		$this->exercice = $exercice;
+	}
+	
+	/// SETTERS ///
+	public set_version($version)
+	{
+		if (is_int($version) && ($version > 0)) $this->version = $version;
+	}
+	public set_auteur($auteur)
+	{
+		if (is_string($auteur) && strlen($auteur)) $this->auteur = $auteur;
+	}
+	public set_exercice(Exercice $exercice)
+	{
+		$this->exercice = $exercice;
+	}
+	
+	/// ISSERS ///
+	
+	/// GETTERS ///
+}
 ?>
